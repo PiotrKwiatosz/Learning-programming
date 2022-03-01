fc5984c20a6c7eb1ecbb07ab64326724d0bbcdb7
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-03-01 19:42:08 +0100

    Merge branch 'master' of https://github.com/PiotrKwiatosz/Learning-programming
    # Conflicts:
    #	SQL w Mgnieniu Oka/03~da8115653ef9b3966f2adeceddc702614c3d3e84

--- SQL w Mgnieniu Oka/05/Zadanie-02.sql
+++ SQL w Mgnieniu Oka/05/Zadanie-02.sql
@@ -1,6 +1,6 @@
 SELECT zam_numer, prod_id, ilosc
 FROM elementyzamowienia
-WHERE ilosc >=100 AND prod_id = 'BR01' OR 'BR02' OR 'BR03'
+WHERE (prod_id = 'BR01' OR 'BR02' OR 'BR03') AND ilosc >=100
 ORDER BY prod_id AND ilosc;
 
  
