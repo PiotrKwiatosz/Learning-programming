edddbcdac524734dd43a0f9d5a97e155c027b1aa
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-03-02 11:57:25 +0100

    Update Zadanie-02

--- SQL w Mgnieniu Oka/05/Zadanie-02.sql
+++ SQL w Mgnieniu Oka/05/Zadanie-02.sql
@@ -1,8 +1,7 @@
 SELECT zam_numer, prod_id, ilosc
 FROM elementyzamowienia
 WHERE (prod_id = 'BR01' OR 'BR02' OR 'BR03') AND ilosc >=100
-ORDER BY prod_id AND ilosc
-;
+ORDER BY prod_id AND ilosc;
 
 
  
