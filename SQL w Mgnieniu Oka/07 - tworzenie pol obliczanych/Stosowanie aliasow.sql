34a6746c076c00da49b6c262f0beb7513384813a
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-03-06 16:22:19 +0100

    Stosowanie aliasow dla wszystkich BD

--- SQL w Mgnieniu Oka/07 - tworzenie pol obliczanych/Stosowanie aliasow.sql
+++ SQL w Mgnieniu Oka/07 - tworzenie pol obliczanych/Stosowanie aliasow.sql
@@ -16,6 +16,7 @@ ORDER BY dost_nazwa;
 
 # Dla MySQL
 
-SELECT Concat(RTrim(dost_nazwa), ' (', RTrim(dost_kraj), '(') AS dost_tytul
+SELECT Concat(dost_nazwa, ' (', dost_kraj, '(') 
+		AS dost_tytul
 FROM Dostawcy
 ORDER BY dost_nazwa;
