dd9540e7ddf29fc1996c6a11c93193404900afbf
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-03-06 13:39:16 +0100

    Dawanie tylko produktow ktore maja w opisie "szmmackanka" i "Rybka"

--- SQL w Mgnieniu Oka/06 - filtrowanie za pomoca znakow wieloznacznych/Zadanie-03.sql
+++ SQL w Mgnieniu Oka/06 - filtrowanie za pomoca znakow wieloznacznych/Zadanie-03.sql
@@ -1,3 +1,4 @@
+# Dawanie tylko produktow ktore maja w opisie "szmmackanka" i "Rybka" #
 SELECT prod_nazwa, prod_opis
 FROM Produkty
 WHERE prod_opis LIKE '%szmacianka%' AND prod_opis LIKE '%Rybka%';
