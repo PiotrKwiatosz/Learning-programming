2d08e9e1c07995384332bb883102d3fdbf9d5765
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-03-25 18:26:14 +0100

    2 instrukcje z filtrowaniem ilosci i prod_id

---
+++ SQL w Mgnieniu Oka/14 - łączenie zapytań/Zadanie-01.sql
@@ -0,0 +1,10 @@
+# Instrukcja obejmujaca 2 instrukcje SELECT pobierajaca prod_id i ilosc. Filtrowanie w 1 ma zwracac tylko ilosc = 100 a w 2 wiersze z prod_id zaczynajacymi sie od BNBG
+
+SELECT prod_id, ilosc
+FROM ElementyZamowienia
+WHERE ilosc = 100
+UNION
+SELECT prod_id, ilosc
+FROM ElementyZamowienia
+WHERE prod_id LIKE 'BNBG%'
+ORDER BY prod_id;
