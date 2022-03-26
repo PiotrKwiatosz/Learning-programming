6a080bfa25c444341fde5e91fc752cc0b1f26e98
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-03-26 19:13:11 +0100

    Instrukcja INSERT

---
+++ SQL w Mgnieniu Oka/15 - wstawianie danych/Wstawianie calych wierszy.sql
@@ -0,0 +1,12 @@
+# Najprostszym sposobem wstawiania danych do tabeli jest instrukcja INSERT
+
+INSERT INTO Klienci
+VALUES ('1000000006',
+	'Zabawkolandia',
+    'Miodowa 12',
+    'Katowice',
+    'SLK',
+    '41-200',
+    'Polska',
+    NULL,
+    NULL);
