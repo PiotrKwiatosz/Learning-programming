d096ec57f6373dbdbafbe18588e5058cc2efda16
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-04-04 07:09:03 +0200

    Sprawdzenie dzialania perspektywy z Zadania-01

---
+++ SQL w Mgnieniu Oka/18 - stosowanie perspektyw/Zadanie-01-sprawdzenie.sql
@@ -0,0 +1,5 @@
+# Utworzenie perspektywy KlienciZZamowieniami zawierajacej wszystkie kolumny z tabeli Klienci, ale obejmujaca tylko wiersze klientow co zlozyli jakies zamowienie.
+# Sprawdzenie stworzonej perspektywy
+
+SELECT *
+FROM KlienciZZamowieniami;
