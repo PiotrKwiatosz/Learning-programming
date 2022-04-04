15462c2c1038917801a82e6bd1d426fae9118d3a
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-04-04 07:01:53 +0200

    Sprawdzenie perspektywy z polami obliczanymi

---
+++ SQL w Mgnieniu Oka/18 - stosowanie perspektyw/Perspektywy z polami obliczanymi-sprawdzenie.sql
@@ -0,0 +1,6 @@
+# Tworzenie perspektywy fktora jest z instrukcja z rozdz. 7.. Pobiera ona elementy wybranego zamowienia wraz z pelna cena za wszystkie sztuki danego elementu
+# Sprawdzenie dzialania dla zamowienia '20008'
+
+SELECT *
+FROM WartoscElementowZamowienia
+WHERE zam_numer = 20008;
