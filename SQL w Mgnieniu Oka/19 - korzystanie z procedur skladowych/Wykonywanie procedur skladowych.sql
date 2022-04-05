302f5bef71750cdd31cdab869e3ef1f9ed49bc64
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-04-05 09:58:21 +0200

    Pozwala na wprowadzenie do tabeli nowego elementu

---
+++ SQL w Mgnieniu Oka/19 - korzystanie z procedur skladowych/Wykonywanie procedur skladowych.sql
@@ -0,0 +1,6 @@
+# Wykonanie procedury skladowej 'DodanieNowegoProduktu'
+
+EXECUTE DodanieNowegoProduktu('JTS01',
+								'Wypchana wieza Eiffela'
+                                24,99,
+                                'Pluszowa zabawka w postaci wiezy z napisem La Tour Eiffel w kolorze czerwonym, bialy, i niebieskim');
