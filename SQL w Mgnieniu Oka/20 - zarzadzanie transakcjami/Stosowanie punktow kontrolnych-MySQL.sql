7739df224a066f939e27e86f554583e44448340d
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-04-06 08:55:49 +0200

    Utworzenie punktow kontrolnych - dla MySQL

---
+++ SQL w Mgnieniu Oka/20 - zarzadzanie transakcjami/Stosowanie punktow kontrolnych-MySQL.sql
@@ -0,0 +1,5 @@
+# Aby umozliwic czesciowe wycofywanie nalezy w bloku transakcji umieszac 'punkty kontrolne' przy anulowaniu jakiegos dzialania wystarczy cofnac sie do wybranego punktu kontrolnego
+
+# Dla MySQL, MariaDB i Oracle
+
+SAVEPOINT usun1;
