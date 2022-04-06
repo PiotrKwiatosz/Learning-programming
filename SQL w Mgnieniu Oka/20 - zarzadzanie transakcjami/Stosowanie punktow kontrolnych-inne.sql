a52f22de5757a834cf453a0ab95e24a43f932f15
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-04-06 08:56:34 +0200

    Utworzenie punktow kontrolnycch - dla innych SZBD

---
+++ SQL w Mgnieniu Oka/20 - zarzadzanie transakcjami/Stosowanie punktow kontrolnych-inne.sql
@@ -0,0 +1,5 @@
+# Aby umozliwic czesciowe wycofywanie nalezy w bloku transakcji umieszac 'punkty kontrolne' przy anulowaniu jakiegos dzialania wystarczy cofnac sie do wybranego punktu kontrolnego
+
+# Dla SQL Server
+
+SAVE TRANSACTION usun1;
