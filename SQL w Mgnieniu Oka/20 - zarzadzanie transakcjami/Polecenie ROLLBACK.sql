26531fdddd6c237863c3ac50e932cb4ec083ff20
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-04-06 08:52:56 +0200

    Polecenie powodujace anulowanie wynikow dzialan instrukcji SQL

---
+++ SQL w Mgnieniu Oka/20 - zarzadzanie transakcjami/Polecenie ROLLBACK.sql
@@ -0,0 +1,6 @@
+# Polecenie 'ROLLBACK' powoduje anulowanie (wycofanie) wynikow dzialan instrukcji SQLowych
+
+# Zostaje przeprowadzona operacja 'DELETE' nastepnie jej dzialanie jest cofniete 'ROLLBACK'
+
+DELETE FROM Zamowieia;
+ROLLBACK;
