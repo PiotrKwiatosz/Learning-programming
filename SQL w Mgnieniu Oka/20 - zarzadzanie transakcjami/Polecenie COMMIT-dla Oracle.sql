d2800de9c9e73a5c03f6cf2b34c81b5f55c7a0c2
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-04-06 08:54:46 +0200

    Wymuszanie jawnego zatwierdzenia - dla Oracle

---
+++ SQL w Mgnieniu Oka/20 - zarzadzanie transakcjami/Polecenie COMMIT-dla Oracle.sql
@@ -0,0 +1,11 @@
+# Zamowienie '12345' jest calkowicie usuwane z bazy. Poniewaz wymaga uaktualnienia 'Zamowienia' i 'ElementyZamowienia'. Stosuje sie blok transakcji w celu wykonania obu polecen.
+# Koncowe 'COMMIT' zapisuje zmiany tylko wtedy gdy nie wystapily zadne bledy
+
+# Dla Oracle
+
+SET TRANSACTION
+DELETE ElementyZamowienia WHERE zam_numer = 12345;
+DELETE Zamowienia WHERE zam_numer 12345;
+COMMIT;
+
+
