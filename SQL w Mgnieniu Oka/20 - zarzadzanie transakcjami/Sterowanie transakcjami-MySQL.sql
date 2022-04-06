ff329a9c320f86e2ef193063f84fd62b81609435
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-04-06 08:52:05 +0200

    Sterowanie transakcjami dla MySQL

---
+++ SQL w Mgnieniu Oka/20 - zarzadzanie transakcjami/Sterowanie transakcjami-MySQL.sql
@@ -0,0 +1,8 @@
+# Wszystkie instrukcje miedzy 'BEGIN TRANSACTION' a 'COMMIT TRANSACTION' musza zostacj wykonane poprawnie w przeciwnym wypadku operacje nalezy anulowac
+
+# '...' - jakis kod SQL
+
+# Dla MySQL i MariaDB
+
+START TRANSACTION
+...
