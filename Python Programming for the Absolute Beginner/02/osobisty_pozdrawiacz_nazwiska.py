d30fd342e9a1bfb9a57314d5743a8a4956121b87
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-04-24 17:06:35 +0200

    Hello for surname

---
+++ Python Programming for the Absolute Beginner/02/osobisty_pozdrawiacz_nazwiska.py
@@ -0,0 +1,10 @@
+# Osobisty pozdrawiacz
+# Demonstruje pobieranie danych wprowadzanych przez użytkownika
+
+surname = input("Cześć. Jak masz na nazwisko   ? ")
+
+print(surname)
+
+print("Cześć,",surname)
+
+input("\n\nAby zakończyć program, naciśnij klawisz Enter.")
