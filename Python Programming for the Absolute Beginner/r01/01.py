3dbdf4ec24735608b4e30c3d79772167f3297a2b
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-04-25 11:10:35 +0200

    Task 01

---
+++ Python Programming for the Absolute Beginner/r01/01.py
@@ -0,0 +1,7 @@
+# Nazwa ulubionych lodow
+
+print("Manhatan i galkowe a potem Big Milki")
+
+# Wygenerowanie swojego oryginalnego bledu poprzez wprowadzenie swojego ulubionego smaku lodow
+
+primt("Waniliowe")
