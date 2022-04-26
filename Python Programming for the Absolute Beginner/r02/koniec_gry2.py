eabd245989c8d8beee45673999fd63267d30dccb
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-04-26 18:19:07 +0200

    Writing 'Koniec gry' version 2.0 - prebuild (without ASCII-Art)

---
+++ Python Programming for the Absolute Beginner/r02/koniec_gry2.py
@@ -0,0 +1,13 @@
+# Koniec gry - wersja 2
+# Demonstruje uzycie cudzyslowow w lancuchach znakow
+
+print("Program 'Koniec gry' 2.0")
+print("Taki sam", "komunikat", "jak przedtem,")
+print("tylko",
+        "nieco",
+        "wiekszy.")
+print("Oto", end=" ")
+print("on...")
+
+print("Napis 'KONIEC GRY' w ASCII-ART")
+input("\n\nAby zakonczyc program, nacisnij klawisz ENTER.")
