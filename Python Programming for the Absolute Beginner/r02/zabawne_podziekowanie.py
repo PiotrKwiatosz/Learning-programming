3e84428d7feedeff50884aa670060ea09c01bee5
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-04-27 18:02:53 +0200

    Funnt thanks

---
+++ Python Programming for the Absolute Beginner/r02/zabawne_podziekowanie.py
@@ -0,0 +1,18 @@
+# Zabawne podziekowania
+# Demonstruje sekwencje specjalne
+
+print("\t\t\tZabawne podziekowania")
+
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\ \\ \\ \\")
+print("\t\t\t       napisal")
+print("\t\t\t   Michael Dawson")
+print("\t\t\t   \\ \\ \\ \\ \\ \\ \\")
+
+print("\nSpecjalne podziekowania naleza sie:")
+print("mojemi fryzjerowi,", end=" ")
+print("Henry\'emu \'Wielkiemu\', ktory nigdy nie mowi \"nie da sie\".")
+
+# dzwonek systemowy
+print("\a")
+
+input("\n\nAby sakonczyc program, nacisnij klawisz Enter.")
