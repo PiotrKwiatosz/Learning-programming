b7ec16d60ad35cb579709e47695b47843ce0e880
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-04-28 18:01:16 +0200

    Stupid chains

---
+++ Python Programming for the Absolute Beginner/r02/glupie_lancuchy.py
@@ -0,0 +1,18 @@
+# Glupie lancuchy
+# Demonstruje konkatencje i powielanie lancuchow
+
+print("Mozesz dokonac konkatencji dwoch " + "lancuchow za pomoca operatora '+'.")
+
+print("\nTen lancuch " + "moze nie " + "sprawiac wiel" + "kiego wrazenia. " \
+      + "Ale " + "pewnie nie wiesz," + " ze jest\n" + "to jeden napraw" \
+      + "d" + "e" + " dlugi lancuch, utworzony przez konkatencje " \
+      + "az " + "dwudziestu dwu\n" + "roznych lancuchow i rozbity na " \
+      + "szesc wierszy." + " Jestes pod" + " wrazeniem tego faktu?\n" \
+      + "Dobrze, ten " + "jeden " + "dlugi" + " lancuch wlasnie sie skonczyl!")
+
+print("\nJesli jakis lancuch naprawde Ci sie podoba, mozesz go powtorzyc.")
+print("Kto na przyklad nie lubi lodow? Masz racje, nikt. Ale jesli naprawde ")
+print("je lubisz, powinienes to wyrazic w adekwatny i mocny sposob:")
+print("Lody!" * 12)
+
+input("\n\nAby zakonczyc program, nacisnij klawisz ENTER.")
