b700ea24000cd240c9717b98a9651f517229fccd
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-04-29 18:26:46 +0200

    Counting task

---
+++ Python Programming for the Absolute Beginner/r02/zadanie_tekstowe.py
@@ -0,0 +1,30 @@
+# Zadania tekstowe 
+# Liczby i dzialania matematyczne
+
+print("Ciezarna hipopotamica wazaca 1500 kg rodzi 45-kilogramowe mlode,")
+print("ale potem zjada 25 kg paszy. Ile wynosi jej nowa waga?")
+input("\nAby sie dowiedziec, nacisnij klawisz ENTER")
+print("1500 - 45 + 25 =", 1500 -45 + 25)
+
+print("\n\nPoszukiwacz przygod wraca z udanej wyprawy i kupuje kazdej ze swoich")
+print("6 towarzyszy 3 butelki piwa. Ile butelek zostalo zakupionych?")
+input("\nAby sie dowiedziec, nacisnij klawisz ENTER")
+print("6 * 3=", 6 * 3)
+
+print("\n\nNaleznosc za obiad w restauracji wynosi razem z napiwkiem 159 zl, a Ty")
+print("postanawiasz ze swoimi przyjaciolmi podzielic ja na 4 rowne czesci. Ile")
+print("kazde z Was bedzie musialo zaplacic?")
+input("\nAby sie dowiedziec, nacisnij klawisz ENTER")
+print("159 / 4=", 159 / 4)
+
+print("\n\nGrupa 4 piratow znajduje skrzynie, a w niej 107 zlotych monet, i")
+print("postanawia podzielic zdobycz po rowno. Ile monet otrzyma kazdy z nich?")
+input("\nAby sie dowiedziec, nacisnij klawisz ENTER")
+print("107 // 4 =", 107 // 4)
+
+print("\n\nTa sama grupa 4 piratow dzieli miedzy siebie po rowno 107 zlotych")
+print("monet ze znalezionyj skrzyni. Ile monet zostanie po podziale?")
+input("\nAby sie dowiedziec, nacisnij klawisz ENTER")
+print("107 % 4 =", 107 % 4)
+
+input("\n\n\nAby zakonczyc program, nacisnij klawisz ENTER")
