1208566bf05b1764081bb2205db4f711ebabe5c9
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-05-01 18:30:41 +0200

    Manipulation of quotes

---
+++ Python Programming for the Absolute Beginner/r02/manipulacje_cytatami.py
@@ -0,0 +1,25 @@
+# Manipulacje cytatami
+# Demonstruje metody lancucha
+
+# Cytat z wypowiedzi prezesa IBM, Thomasa Watsona, z 1943 r.
+quote = "Myślę, że istnieje światowy rynek dla może pięciu komputerów."
+
+print("Oryginalny cytat w tlumaczeniu na jezyk polski:")
+print(quote)
+
+print("\nDuzymi literami:")
+print(quote.upper())
+
+print("\nMalymi literami:")
+print(quote.lower())
+
+print("\nWszystkie wyrazy od duzej litery:")
+print(quote.title())
+
+print("\nZ drobna zamiana:")
+print(quote.replace("pięciu", "milionów"))
+
+print("\nOryginalny cytat pozostaje bez zmian")
+print(quote)
+
+input("\n\nAby zakonczyc program, nacisnij ENTER")
