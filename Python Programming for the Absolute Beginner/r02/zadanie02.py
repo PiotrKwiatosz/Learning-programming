56443c8d33c20b03a8f267d96a497a2d96c775e7
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-05-10 17:13:06 +0200

    Task 02

---
+++ Python Programming for the Absolute Beginner/r02/zadanie02.py
@@ -0,0 +1,15 @@
+# Rozdzial 2
+# Zadanie 2
+
+# Program ktory umozliwia uzytkownikowi wprowadzenie nazwy 2 ulubionych przysmakow. Potem program wypisuje naze nowego przysmaku utworzonego przez polaczenie podanych nazw przez uzytkownika
+
+print("Czesc, podaj mi dwie nazy swoich ulubionyvh przysmakow a stworze z nich cos")
+
+name1 = input("Prosze podaj nazwe pierwszego przysmaku: ")
+name2 = input("Super juz mamy pierwszy! Teraz podaj mi nazwe drugiego przysmaku: ")
+
+przysmak = name1 + name2
+
+print("Okej, oto i to co stworzylem:", przysmak, "\nPrawda ze super mix?")
+
+input("\n\nAby zakonczyc nacisnij klawisz ENTER")Ba
