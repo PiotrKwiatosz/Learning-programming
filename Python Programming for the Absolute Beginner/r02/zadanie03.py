c1d41f4d93b5427f8e52025f3ee481559a704742
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-05-10 17:14:08 +0200

    Task 03

---
+++ Python Programming for the Absolute Beginner/r02/zadanie03.py
@@ -0,0 +1,18 @@
+# Rozdzial 2
+# Zadanie 3
+
+# Kalkulator napiwku
+
+print("Witaj! Jestem kalkulatorem napiwku, ktory oblicza jaki napiwek powinienes dac. W wysokosci 15% albo 20%")
+
+rachunek = input("Prosze podaj ogolna sume z rachunku wystawionego przez restauracje: ")
+rachunek = int(rachunek)
+
+napiwek_1 = rachunek * 0.15
+napiwek_2 = rachunek * 0.25
+
+print("Oto napiwki jakie powinienes dac, wybor nalezy do Ciebie: ")
+print(napiwek_1)
+print(napiwek_2)
+
+input("\n\nAby zakonczyc program, nacisnij klawisz ENTER")
