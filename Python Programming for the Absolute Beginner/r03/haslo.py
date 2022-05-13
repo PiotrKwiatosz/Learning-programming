6b39a5ac631022ff6b4b95e7deeba8086e3d3292
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-05-13 18:28:28 +0200

    Password

---
+++ Python Programming for the Absolute Beginner/r03/haslo.py
@@ -0,0 +1,13 @@
+# Haslo
+# Demonstruje instrukcje if
+
+print("Witaj w systemie firmy Bezpieczny Komputer SA")
+print("-- bezpieczenstwo to podstawa naszego dzialania\n")
+
+password = input("wprowadz haslo: ")
+
+if password == "sekret":
+    print("Dostep zostal udzielony")
+    print("\nWitaj przyjacielu!")
+
+input("\n\nAby zakonczyc program, nacisnij ENTER")
