bca5a7273c740c3dd274b7118c67381b188f8862
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-05-14 18:13:00 +0200

    Granted-Refused access

---
+++ Python Programming for the Absolute Beginner/r03/udzielony_odmowiony.py
@@ -0,0 +1,14 @@
+# Haslo
+# Demonstruje instrukcje if
+
+print("Witaj w systemie firmy Bezpieczny Komputer SA")
+print("-- bezpieczenstwo to podstawa naszego dzialania\n")
+
+password = input("wprowadz haslo: ")
+
+if password == "sekret":
+    print("Dostep zostal udzielony")
+else:
+    print("Odmowa dostepu") 
+
+input("\n\nAby zakonczyc program, nacisnij ENTER")
