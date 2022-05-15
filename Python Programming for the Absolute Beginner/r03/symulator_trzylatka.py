ace9fa0d00f136d1d8e8b84ee727f7c7d252c57c
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-05-15 17:40:56 +0200

    A three-year-old simulator

---
+++ Python Programming for the Absolute Beginner/r03/symulator_trzylatka.py
@@ -0,0 +1,14 @@
+# Symulator trzylatka
+# Demonstruje petle "while"
+
+print("\tWitaj w 'Symulatorze trzylatka'\n")
+print("Ten program symuluje rozmowe z trzyletnim dzieckiem.")
+print("Sprobuj przerwac to szalensto.\n")
+
+response = ""
+while response != "Dlatego":
+    response = input("Dlaczego?\n")
+
+print("Aha. Juz wiem.")
+
+input("\n\nAby zakonczyc program, nacisnij klawisz ENTER")
