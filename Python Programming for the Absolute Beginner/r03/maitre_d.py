219f6fb9cfe8c73471463e53e6f91db7cba6a20e
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-05-17 11:54:10 +0200

    True or False

---
+++ Python Programming for the Absolute Beginner/r03/maitre_d.py
@@ -0,0 +1,14 @@
+# Maitre d'
+# Demonstruje traktowanie wartosci jako warunku
+
+print("Witaj w Chateau D' Smakosz")
+print("Wydaje sie, ze tego wieczoru mamy prawie komplet gosci.\n")
+
+money =int(input("Ile zlotych wsuniesz do kieszeni maitre d'? "))
+
+if money:
+    print("Och, przypomnialem sobie o wolnym stoliku. Prosze tedy.")
+else:
+    print("Prosze zaczekac. To moze troche potrwac")
+
+input("\n\nAby zakonczyc program, nacisnij klawisz ENTER")
