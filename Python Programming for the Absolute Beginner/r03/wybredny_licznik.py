95ad0796cf15a7bdb024fbc2ab80c1eaf1afc706
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-05-20 21:00:35 +0200

    Count only to 10 and skip 5

---
+++ Python Programming for the Absolute Beginner/r03/wybredny_licznik.py
@@ -0,0 +1,15 @@
+# Wybredny licznik
+# Demonstruje instrukcje 'break' i 'continue'
+
+count = 0
+while True:
+    count += 1
+    # zakoncz petle jesli wartosc zmiennej count jest wieksza niz 10
+    if count > 10:
+        break
+# poomin liczbe 5
+    if count == 5:
+        continue
+    print(count)
+
+input("\n\nAby zakonczyc program, nacisnij klawisz ENTER")
