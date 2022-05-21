1dab8fb8271cf720c3aeb9a03da148958a4fd69e
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-05-21 20:20:01 +0200

    Exclusive network

---
+++ Python Programming for the Absolute Beginner/r03/ekskluzywna_siec.py
@@ -0,0 +1,38 @@
+# Ekskluzywna siec
+# Demonstruje operatory logiczne i warunki zlozone
+
+from re import T
+
+
+print("\tEkskluzywna Siec Komputerowa")
+print("\t   Tylko dla czlonkow!\n")
+
+security = 0
+
+username = ""
+while not username:
+    username = input("Uztkownik: ")
+
+password = ""
+while not password:
+    password = input("Haslo: ")
+
+if username == "M.Dawson" and password == "sekret":
+    print("Czesc, Mike!")
+    security = 5
+elif username == "S.Meier" and password == "cywilizacja":
+    print("Hej, Sid!")
+    security = 3
+elif username == "S.Miyamoto" and password == "mariobros":
+    print("Co u Ciebie, Shigeru?")
+    security = 3
+elif username == "W.Wright" and password == "simsowie":
+    print("Jak leci, Will?")
+    securitiy = 3
+elif username == "gosc" or password == "gosc":
+    print("Witaj, Gosciu!")
+    security = 1
+else:
+    print("Nieudana proba logowania. Nie jestes taki wyjatkowy.\n")
+
+input("\n\nAby zakonczyc program, nacisnij klawisz ENTER.")
