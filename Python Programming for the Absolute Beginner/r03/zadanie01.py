fc906e1418b0a7577e76cbc80176577714e061c8
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-05-23 17:56:38 +0200

    Task 01

---
+++ Python Programming for the Absolute Beginner/r03/zadanie01.py
@@ -0,0 +1,36 @@
+# Program ktory symuluje ciasteczko z wrozba.
+# Program powinien wyswietlic 1 z 5 niepowtarzalnych przepowiednim,
+# dokonujac losowego wyboru przy kazdym uruchomieniu
+
+import random
+
+print("Witaj w wirtualnym 'Ciasteczku z Przepowiedniami'!")
+print("Ugryz (-> Nacisnij klawisz ENTER <-) ciasteczko i sprawdz przepowiednie.\n")
+input("Twoja ciasteczkowa przepowiednia to: \n")
+
+cookie = random.randint(1, 5)
+
+if cookie == 1:
+    # przepowiednia 1
+    print("\tNie stawiaj czoła rzeczywistości. Odbij się od niej.")
+
+elif cookie == 2:
+    # przepowiednia 2
+    print("\tKto urodził się kurą, orłem nie umrze.")
+
+elif cookie == 3:
+    # przepowiednia 3
+    print("\tSzczęście, którego szukasz, jest w drugim ciasteczku.")
+
+elif cookie == 4:
+    print("\tGdy widzisz mędrca, myśl o tym, by mu dorównać. Gdy widzisz tego, komu brak rozsądku, zastanów się nad samym sobą.")
+
+elif cookie == 5:
+    print("\tNie oglądaj się za siebie.")
+
+else:
+    print("\tUgryz ciasteczko, bo ucieknie!")
+
+input("\n\nAby zakonczyc program, nacisnij klawisz ENTER.")
+
+
