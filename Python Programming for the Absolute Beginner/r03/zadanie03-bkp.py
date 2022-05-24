9e3b7302d5943936eeaa27fd90d0fa1f183ce2c5
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-05-24 19:47:55 +0200

    Almost good

---
+++ Python Programming for the Absolute Beginner/r03/zadanie03-bkp.py
@@ -0,0 +1,34 @@
+# Zmodyfikowany program 'Jaka to liczba' tak, aby gracz dysponowal ograniczona 
+# liczba prob odgadniecia wybranej przez komputer liczby.
+# Gdy graczowi nie uda sie odgadnac tej liczby w wyznaczonej liczbie prob, program powinien wyswietlic odpowiedni komunikat z reprymenda.
+
+import random
+
+print("\tWitaj w grze 'Jaka to liczba'! wersja HARDCORE!")
+print("\nMam na mysli pewna liczbe z zakresu od 1 do 100.")
+print("Sprobuj ja odgadnac w jak najmniejszej liczbie prob.\n")
+print("Masz na to tylko 10 prob!\n")
+
+# ustaw wartosci poczatkowe
+the_number = random.randint(1,100)
+guess = int(input("Ta liczba to: "))
+tries = 10
+
+# petla zgadywania
+while guess != the_number:
+    if guess > the_number:
+        print("Za duza...")
+    elif guess < the_number:
+        print("Za mala...")
+    elif guess == the_number:
+        print("Odgadles! Ta liczba to:", the_number)
+        print("Do osiagniecia sukcesu potrzebowales tylko", tries, "z 10 prob!\n")
+
+    guess = int(input("Ta liczba to: "))
+    tries -= 1
+
+    if tries == 0:
+        print("KONIEC! Nie udalo Ci sie zgadnac w 10 probach...")
+        break
+
+input("\n\nAby zakonczyc program, nacisnij klawisz ENTER.")
