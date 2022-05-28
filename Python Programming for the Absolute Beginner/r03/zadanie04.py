6f6dd50b1a2393ce3d3a11bce9501bbfdd30b5b8
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-05-28 13:34:11 +0200

    Task 04 - 2nd version

--- Python Programming for the Absolute Beginner/r03/zadanie04.py
+++ Python Programming for the Absolute Beginner/r03/zadanie04.py
@@ -12,34 +12,37 @@ print("Ale mam na to tylko 10 prob")
 import random
 
 number = random.randint(1,100)
-guess = int(input("Czy ta liczba to: ?"))
-
-answer1 = ""
-while not answer1:
-    answer1 = input("Tak")
-answer2 = ""
-while not answer2:
-    answer = input("Nie")
-    
+guess = print("Czy ta liczba to: ", number, " ?")
 tries = 10
+
+answer = ""
+while not answer:
+    answer = input("Tak?")
+     
+while not answer:
+    answer = input("Nie?")
+
 
 while tries > 0:
     while number != guess:
-            print("Czy ta liczba to: ", number, "?")
-            if answer == "Nie":
-                print("Za duza?")
-                if input(answer1):
+        print("Czy ta liczba to: ", number, "?")
+        if answer == "Nie":
+            print("Za duza?")
+            if answer == "Tak":
+                print("Czy ta liczba to: ", number ,"?")
+            elif answer == "Nie":
+                print("Za mala?")
+                if answer == "Tak":
                     print("Czy ta liczba to: ", number, " ?")
-                elif input(answer2):
-                    print("Czy ta liczba to ", number, " ?")
-            elif answer == "Tak":
-                print("Udalo mi sie! Twoja liczba to: ", number, "Super bylo!")
-                print("Aby ja odgadnac porzebowalem ", 10 - tries, " prob!")
-                break
-            else:
-                print("Nie rozumiem Cie...")
+        elif answer == "Tak":
+            print("Udalo mi sie! Twoja liczba to: ", number, "Super bylo!")
+            print("Aby ja odgadnac porzebowalem ", 10 - tries, " prob!")
+            break
+        else:
+            print("Nie rozumiem Cie...")
+            break
 
-    guess = print("Czy ta liczba to: ?")
+   # guess = print("Czy ta liczba to: ", number, " ?")
     tries -= 1
 
 if tries == 0:
