d85a63a01bdb2b83a2cee8a3fd1d37c28e9c498c
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-05-31 15:48:59 +0200

    Crazy chain

---
+++ Python Programming for the Absolute Beginner/r04/zwariowany_lancuch.py
@@ -0,0 +1,11 @@
+# Zwariowany lancuch
+#
+# Demonstruje uzycie petli 'for' z lancuchem znakow
+
+word = input("Wprowadz jakies slowo: ")
+
+print("\n Oto poszczegolne litery Twojego slowa:")
+for letter in word:
+    print(letter)
+
+input("\n\nAby zakonczyc program, nacisnij klawisz ENTER")
