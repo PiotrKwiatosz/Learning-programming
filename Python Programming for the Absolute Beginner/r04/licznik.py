a269b864cf1ce863d13179d598a5f9a6254cca2c
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-06-02 16:39:22 +0200

    Counter

---
+++ Python Programming for the Absolute Beginner/r04/licznik.py
@@ -0,0 +1,16 @@
+# Licznik
+# Demonstruje funkcje 'range()'
+
+print("Liczenie:")
+for i in range(10):
+    print(i, end=" ")
+
+print("\n\nLicznenie co piec:")
+for i in range(0, 50, 5):
+    print(i, end=" ")
+
+print("\n\nLiczenie do tylu:")
+for i in range(10, 0, -1):
+    print(i, end=" ")
+
+input("\n\nAby zakonczyc program, nacisnij klawisz ENTER")
