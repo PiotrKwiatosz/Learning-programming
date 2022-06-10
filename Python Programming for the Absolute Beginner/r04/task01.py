e144aa5bd6eb780dc04c0757b6adfa94a071a6d7
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-06-10 12:21:32 +0200

    Counter with separator

---
+++ Python Programming for the Absolute Beginner/r04/task01.py
@@ -0,0 +1,15 @@
+# Program ktory liczy za uzytkownika
+# Wprowadza sie liczbe poczatkowa i startowa oraz odstep miedzy liczbami
+
+print("Witaj, oto program ktory liczy za Ciebie, odkad dokad chcesz")
+print("Wprowadz liczbe poczatkowa i koncowa oraz separator co ile:")
+
+start = (input("\nPoczatek: "))
+start = int(start)
+finish = int(input("Koniec: "))
+separator = int(input("Co ile ma byc liczone: "))
+
+for i in range(start, finish, separator):
+    print(i, end=" ")
+
+input("\n\nAby zakonczyc program, nacisnij klawisz ENTER.")
