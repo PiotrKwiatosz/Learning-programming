ea69166c84a74862682acc41b2fd50268a79fba2
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-06-13 18:12:57 +0200

    Task 02

---
+++ Python Programming for the Absolute Beginner/r04/zadanie02.py
@@ -0,0 +1,10 @@
+# Program ktory wczytuje komunikat od uzytkownika
+# Nastepnie wypisuje go w odwrotnej kolejnosci
+
+message = input("Wprowadz swoj komunikat: ")
+
+print("\nTen komunikat w odwrotnej kolejnosci to: ")
+print("\n", message[::-1])
+
+
+input("\n\nAby zakonczyc program, nacisnij klawisz ENTER.")
