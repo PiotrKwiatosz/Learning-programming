78ccab7c99a17b488370282f836b340eeb98a1bb
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-07-11 18:57:17 +0200

    Task 02 - BETA VERSION - v.1.0

--- Python Programming for the Absolute Beginner/r05/zadanie02.py
+++ Python Programming for the Absolute Beginner/r05/zadanie02.py
@@ -1,117 +1,51 @@
 # Napisz program 'Kreator postaci' do gry RPG (z podzalem na role).
 # Gracz powinien otrzymac pule 30 punktow, ktore moze spozytkowac na 4 atrybuty:
-# -sila, -zdrowie, -madrosc, -zrecznosc
+# -Sila, -Zdrowie, -Madrosc, -Zrecznosc
 # Gracz powinien miec mozliwosc przeznaczanoa punktow z puli na dowolny atrybut, 
 # jak rowniez mozliwosc odbierania punktow przypisanych do atrybutu i
 # przekazywanie ich spowrotem do puli
 
-points = []
 
-choice = None
-while choice != "O":
-
-    print(
-        """
+print("""
+                KREATOR POSTACI DO MAGICZNEGO SWIATA!
         
-        Kreator postaci do magicznego swiata!
         Dostajesz -> 30 <- punktow ktore mozesz rozdysponowac na 4 wartosci
-        -sila, -zdrowie, -madrosc i -zreczonsc.
-        Wybierz co chcesz zrobic:
-
-        0 - zakoncz
-        1 - wyswietl na co przyznales punkty
-        2 - wybierz kategorie na co chcesz dodac punkty
-        """
+            -Sila, -Zdrowie, -Madrosc i -Zreczonsc.
+        Jesli zmienisz zdanie, mozesz zabrac punkty z dangeo atrybutu i dac je spowrotem
+        do magazynu.
+"""
     )
 
-    choice = input("Wybierasz: ")
-    print()
+attribute = {'strength':0, 'health':0, 'wisdom':0, 'dexterity':0}
+points = 30
+choice = None
+spend = None
 
-    # zakoncz
-    if choice == "0":
-        print("Zegnaj!")
+choice_sentence = """
+\n\nWybierz co chcesz zrobic:
 
-    # wyswietl tabele na co przyznales punkty
-    elif choice == "1":
-        print("Punkty przyznane na\n: ")
-        print("WARTOSC\tPUNKTY")
-        for entry in points:
-            points, name = entry
-            print(name, "\t", point)
+        0 - Zakoncz
+        1 - Wyswietl na co przyznales punkty
+        2 - Wybierz kategorie na co chcesz dodac punkty
+"""
 
-    # dodaj punkty
-    elif choice == "2":
-        print(
-            """
-            
-            Wartosci na ktore mozesz przeznaczyc punkty:
-            
-            0 - wyjdz
-            1 - SILA
-            2 - ZDROWIE
-            3 - MADROSC
-            4 - ZRECZNOSC
-            5 - Usuwanie punktow
-            """
-        )
-    choice = None
-    while choice != "0":
+attribute_list = """
+\tStrength
+\tHealth
+\tWisdom
+\tDexterity
+"""
 
-        choice = input("Wybierasz: ")
-        print()
-        used = []
-        points != 30
+while choice != "0":
 
-        # wyjdz
-        if choice == "0":
-            print("Powrot do glownego menu")
+    print("\nTwoj charakter ma takie atrybuty:")
+    for attribute, points in attributes.items():
+        print("\t", attribute.title(), ":\t ", points)
 
-        # wybrana SILA
-        elif choice == "1":
-            point = int(input("Ile punktow chcesz przeznaczyc na SILE? :"))
-            strenght = point
-            points.append(strenght)
-            points.sort(reverse=True)
-            points = points[:30]
-            used.append(points)    
-        
-         # wybrana ZDROWIE
-        elif choice == "2":
-            point = int(input("Ile punktow chcesz przeznaczyc na ZDROWIE? :"))
-            health = point
-            points.append(health)
-            points.sort(reverse=True)
-            points = points[:30]
-            used.append(points)
 
-        # wybrana MADROSC
-        elif choice == "3":
-            point = int(input("Ile punktow chcesz przeznaczyc na MADROSC? :"))
-            wisdom = point
-            points.append(wisdom)
-            points.sort(reverse=True)
-            points = points[:30]
-            used.append(points)
-
-        # wybrana ZRECZNOSC
-        elif choice == "4":
-            point = int(input("Ile punktow chcesz przeznaczyc na ZRECZNOSC? :"))
-            skill = point
-            points.append(skill)
-            points.sort(reverse=True)
-            points = points[:30]
-            used.append(points)
-
-        elif choice == "5":
-            print("Skad chcesz usunac punkty? :")
-
-        elif points == 0:
-            print("Niestety nie masz juz punktow")
-            break
-
-        # nieznana opcja
-        else:
-            print("Niestety,", choice, "nie jest prawidlowa")
+# nieznana opcja
+    else:
+        print("Niestety,", choice, "nie jest prawidlowa")
             
     
 input("\n\nAby zakonczyc program, nacisnij klawisz ENTER.")
