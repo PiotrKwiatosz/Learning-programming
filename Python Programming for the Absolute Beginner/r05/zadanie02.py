ca6c4d749f8057b34aa41d146f9eb6d8ba3d5f39
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-07-13 18:43:17 +0200

    Task 02 - ALPHA - 13.07.2022

--- Python Programming for the Absolute Beginner/r05/zadanie02.py
+++ Python Programming for the Absolute Beginner/r05/zadanie02.py
@@ -9,14 +9,14 @@
 print("""
                 KREATOR POSTACI DO MAGICZNEGO SWIATA!
         
-        Dostajesz -> 30 <- punktow ktore mozesz rozdysponowac na 4 wartosci
-            -Sila, -Zdrowie, -Madrosc i -Zreczonsc.
+        Dostajesz -> 30 <- punktow ktore mozesz rozdysponowac na 4 wartosci:
+            Sila, Zdrowie, Madrosc i Zreczonsc.
         Jesli zmienisz zdanie, mozesz zabrac punkty z dangeo atrybutu i dac je spowrotem
         do magazynu.
 """
     )
 
-attribute = {'strength':0, 'health':0, 'wisdom':0, 'dexterity':0}
+attributes = {'strength':0, 'health':0, 'wisdom':0, 'dexterity':0}
 points = 30
 choice = None
 spend = None
@@ -24,9 +24,9 @@ spend = None
 choice_sentence = """
 \n\nWybierz co chcesz zrobic:
 
-        0 - Zakoncz
-        1 - Wyswietl na co przyznales punkty
-        2 - Wybierz kategorie na co chcesz dodac punkty
+0 - Zakoncz
+1 - Wyswietl na co przyznales punkty
+2 - Wybierz kategorie na co chcesz dodac punkty
 """
 
 attribute_list = """
@@ -41,11 +41,42 @@ while choice != "0":
     print("\nTwoj charakter ma takie atrybuty:")
     for attribute, points in attributes.items():
         print("\t", attribute.title(), ":\t ", points)
+    print("\nJest ", points, "punktow w schowku.")
+    print(choice_sentence)
+    choice = input("Wybierasz: ")
+    print()
+
+    while choice != "0" and choice != "1" and choice !="2":
+        print("Zly wybor")
+        print(choice_sentence)
+        choice = input("Wybierasz: ")
+    
+    # dodanie punktow do atrybutu
+    while choice == "1":
+        if points == 0:  # nie ma juz punktow
+            print("Przykro mi, ale nie masz punktow. Sprobuj dodac tu troche punktow")
+            break
+        print("\nMasz ", points, " punktow w schowku")
+        print("\nNa jaki atrybut chcesz je przyznac?")
+        print(attribute_list)
+        att_to_change = input("Atrybut do zmienienia: ")
+        while (att_to_change != attribute_list):
+            print("To jest nieprawidlowy wybor.")
+            print("Na ktory atrybut chcesz dodac punkty?")
+            print(attribute_list)
+            att_to_change = input("Atrybut do zmiany: ")
+
+
+    if choice == "0":
+        print("\nZegnaj.")
+
+
+
 
 
 # nieznana opcja
-    else:
-        print("Niestety,", choice, "nie jest prawidlowa")
+#    else:
+#        print("Niestety,", choice, "nie jest prawidlowa")
             
     
 input("\n\nAby zakonczyc program, nacisnij klawisz ENTER.")
