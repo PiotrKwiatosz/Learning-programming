8b946d76c5835bd2c30490a6dc6af7e8459a6af1
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-09-02 13:25:08 +0200

    08 - Flask Forms - 02.08.2022

--- Flask Course - Python Web Application Development/08/market/forms.py
+++ Flask Course - Python Web Application Development/08/market/forms.py
@@ -1,11 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='Nazwa uzytkownika:', validators=[Length(min=2, max=30), DataRequired()])
-    email_address = StringField(label='Adres email:', validators=[Email(), DataRequired()])
-    password1 = PasswordField(label='Haslo:', validators=[Length(min=6), DataRequired()])
-    password2 = PasswordField(label='Powtorz haslo:', validators=[EqualTo('password1'), DataRequired()])
+    username = StringField(label='Nazwa uzytkownika:')
+    email_address = StringField(label='Adres Email:')
+    password1 = PasswordField(label='Haslo:')
+    password2 = PasswordField(label='Powtorz haslo:')
     submit = SubmitField(label='Stworz konto!')
