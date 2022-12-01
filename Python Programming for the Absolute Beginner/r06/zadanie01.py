36a224695bd5a7a93ed28bf33525b070e64a0fd2
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-12-01 20:11:08 +0100

    Task 01 - UPDATE 01.12.2022

--- Python Programming for the Absolute Beginner/r06/zadanie01.py
+++ Python Programming for the Absolute Beginner/r06/zadanie01.py
@@ -41,7 +41,7 @@ def ask_yes_no(question):
 #
 def ask_number(question, low, high, step = 1):
     """Popros o podanie liczy z odpowiedniego zakresu."""
-    print("Krok numer: ", step)
+    print("Krok numer: ",)
     response = None
     while response not in range(low, high, step):
         response = int(input(question))
