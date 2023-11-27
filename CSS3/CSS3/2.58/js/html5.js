647be4d3f410a6074994a89e8078f16925b5ca52
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2023-11-27 20:03:09 +0100

    HTML page template - rescaling and rotating images - part 1

---
+++ CSS3/CSS3/2.58/js/html5.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+Document.createElement('article');
+Document.createElement('header');
+Document.createElement('footer');
+Document.createElement('aside');
+Document.createElement('nav');
+Document.createElement('hgroup');
+Document.createElement('section');
