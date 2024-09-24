0082dbf6f3709e90c197873dd56158c38f9aef02
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2024-09-24 19:43:43 +0200

    Task 02 - working at this v2

--- Python Programming for the Absolute Beginner/r07/zadanie02.py
+++ Python Programming for the Absolute Beginner/r07/zadanie02.py
@@ -3,6 +3,7 @@
 # Program rejestruje nazwe gracza i jego wynik, jesli miesci sie na liscie
 
 import sys
+from high_score import high_scores
 
 def open_file(file_name, mode):
     """Otworz plik."""
@@ -39,40 +40,7 @@ def next_block(the_file):
    
     explanation = next_line(the_file)
 
-    return category, question, answers, score_list, correct, explanation
-
-##
-#  def high_scores(score):
-    import pickle, shelve
-    """Zapisuje nazwe gracza i jego wynik jesli jest wystarczajaco duzy"""
-    print("Zapisywanie wysokich wynikow")
-
-    win = score
-
-    for scores in high_scores:
-        (score, name) = scores
-        if win >= int(score):
-            got_a_high_score = True
-            name = input("Zdobyles wysoki wynik! Jak sie nazywasz?")
-            print("Dobra robota ", name, "! Twoj wynik to: ", win, "!", sep = "")
-            high_scores.sort()
-            high_scores.pop(0)
-            high_scores.append((win, name))
-            high_scores.sort(reverse = True)
-            high_scores = open("high_scores_pickle.dat", "wb")
-            pickle.dump(high_scores, high_score)
-            break
-    if got_a_high_score == False:
-        print("Przepraszam, nie zdobyles dostatecznie wysokiego wynikum probuj jeszcze raz!")
-
-    f = open("high_scores_pickle.dat", "wb")
-    high_score = open_file("high_scores_pickle.dat", "rb")
-    high_scores = pickle.load(high_scores)
-    high_score.close()
-    high_scores.sort(reverse = True)
-    got_a_high_score = False
-    high_score.close()
-    ##
+    return category, question, answers, score_list, explanation
 
 def welcome(title):
     """Przywitaj gracza i pobierz jego nazwe."""
@@ -86,7 +54,7 @@ def main():
     score = 0
 
     # pobierz pierwszy blok
-    category, question, answers, points, correct, explanation = next_block(trivia_file)
+    category, question, answers, score_list, points, correct, explanation = next_block(trivia_file)
 
     while category:
         # zadaj pytanie
@@ -112,14 +80,14 @@ def main():
         score = score + score_list[int(answer) - 1]
 
         # pobierz kolejny blok
-        category, question, answers, points, correct, explanation = next_block(trivia_file)
-
-    high_scores(score)
+        category, question, answers, score_list, points, correct, explanation = next_block(trivia_file)
 
     trivia_file.close()
 
     print("To bylo ostatnie pytanie!")
     print("Twoj koncowy wynik wynosi", score)
+
+    high_scores(score)
 
 main()
 input("\n\nAby zakonczyc program, nacisnij klawisz ENTER") 
